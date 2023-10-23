3f33516e48eb4363e8ba4f9f9fa17b64f378f9a6
KLisabeth <katie_lisabeth@yahoo.com>
2023-10-23 09:28:47 +0200

    installation

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import './assets/main.scss';
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router';
+
+import App from './App.vue';
+
+
+const app = createApp(App);
+
+app.use(createPinia());
+
+
+app.use(router);
+
+app.mount('#app');

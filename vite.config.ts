0731e0c74a93310d492665863c03ae44921c5505
KLisabeth <katie_lisabeth@yahoo.com>
2024-02-09 16:20:43 +0100

    more configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import dts from "vite-plugin-dts";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import nightwatchPlugin from 'vite-plugin-nightwatch'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({

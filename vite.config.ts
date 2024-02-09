5100ca47f683588d8d95d85d6ada9e7e94e7aaa6
KLisabeth <katie_lisabeth@yahoo.com>
2024-02-09 14:57:46 +0100

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/https://klisabeth.github.io/booking-app/",
   plugins: [
     vue(),
     vueJsx(),
